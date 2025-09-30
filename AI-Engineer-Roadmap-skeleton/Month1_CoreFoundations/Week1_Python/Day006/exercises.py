c41d3fee07f873b2e98a13370bb1764f44de44f0
HITESH SHARMA <hiteshsharma1175@gmail.com>
2025-09-30 21:13:33 +0530

    Update exercises.py
    #

--- AI-Engineer-Roadmap-skeleton/Month1_CoreFoundations/Week1_Python/Day006/exercises.py
+++ AI-Engineer-Roadmap-skeleton/Month1_CoreFoundations/Week1_Python/Day006/exercises.py
@@ -1,9 +1,9 @@
 # Day 6 exercises - placeholder
 # Topic: Month1_CoreFoundations / Week1_Python
 
-# def main():
-#     print("Add your code for Day 6 here")
+def main():
+    print("Add your code for Day 6 here")
 
-# print (main) 
+ print (main) 
 
 print("hello")
